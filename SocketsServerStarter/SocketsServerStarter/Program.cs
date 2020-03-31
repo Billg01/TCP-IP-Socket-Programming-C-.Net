1a12a36a0324037845e3ad6df832d11d2d91e757
William Gall <willliom007@gmail.com>
2020-03-31 10:35:42 -0400

    First commit for starting socket connection

---
+++ SocketsServerStarter/SocketsServerStarter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketsServerStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            listenerSocket.Bind(ipep);
+
+            listenerSocket.Listen(5);
+
+            Socket client  = listenerSocket.Accept();
+
+            Console.WriteLine("Client connected. " + client.ToString() + " - IP End Point: " + client.RemoteEndPoint.ToString());
+
+            byte[] buff = new byte[128];
+
+            int numberOfReceivedBytes = 0;
+
+            numberOfReceivedBytes = client.Receive(buff);
+
+            Console.WriteLine("Number of received byte: " + numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent by client is: " + buff);
+
+        }
+    }
+}
